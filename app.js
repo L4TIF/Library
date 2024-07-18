90f1a4806e4893985daa1dc5c9ba9ef3121376f3
Latiful Rahman <latif.fiverr@gmail.com>
2024-07-18 16:58:41 +0000

    working added

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const addBtn = document.querySelector(".add");
 const dialog = document.querySelector("dialog");
 const closeDialog = document.querySelector(".close")
 const submitBtn = document.querySelector(".submit");
-let title = "";
-let author = "";
-let pages = "";
-let read = "";
+let title = "Game Of Thrones";
+let author = "Unknown";
+let pages = "500";
+let read = "Completed";
 // array of books
 const myLibrary = [];
 
@@ -16,30 +16,39 @@ function book(title, author, pages, read) {
     this.read = read;
 }
 
-
-
 function addBookToLibrary() {
     let newBook = new book(title, author, pages, read);
     myLibrary.push(newBook)
-    createCard(newBook);
+    traverseLibrary();
+}
+addBookToLibrary();  //calling it first time so there will be one card by default.
 
+
+
+
+function traverseLibrary() {
+    document.querySelector(".cards").innerHTML = "";
+    for (const iterator of myLibrary) {
+        console.log(iterator)
+        createCard(iterator)
+    }
 }
 
-function createCard(newBook) {
-    let cardHtml = `
- 
+// creating card
+function createCard(iterator) {
+    let cardHtml = `  
     <div class="content">
-          <label for="title">Title<h2 class="title">${newBook.title}</h2></label>
-          <label for="author">Author<p class="author">${newBook.author}</p></label>
-          <label for="pages">Pages<p class="pages">${newBook.pages}</p></label>
-          <div class="btn"><button class="read">${newBook.read}</button>  <button class="Edit">Edit</button></div>
+          <label for="title">Title<h2 class="title">${iterator.title}</h2></label>
+          <label for="author">Author<p class="author">${iterator.author}</p></label>
+          <label for="pages">Pages<p class="pages">${iterator.pages}</p></label>
+          <div class="btn"><button class="read">${iterator.read}</button>  <button class="Edit">Edit</button></div>
     </div>`;
     console.log(myLibrary)
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = cardHtml;
-    console.log(card)
     document.querySelector(".cards").appendChild(card);
+
 }
 
 function getReadStatus() {
@@ -50,12 +59,21 @@ function getReadStatus() {
     }
 }
 
+
+// resetting form after clicking OK
+function resetForm() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#pages").value = "";
+}
+
 addBtn.addEventListener("click", () => {
     dialog.showModal();
 })
 
 closeDialog.addEventListener("click", () => {
     dialog.close();
+    resetForm();
 })
 
 submitBtn.addEventListener("click", () => {
@@ -63,6 +81,13 @@ submitBtn.addEventListener("click", () => {
     author = document.querySelector("#author").value;
     pages = document.querySelector("#pages").value;
     read = getReadStatus();
-    addBookToLibrary();
+    if ((title && author && pages)) {
+        addBookToLibrary();
+        resetForm();
+        dialog.close();
+    }
+    else
+        window.alert("Enter all fields")
+
 })
 
